a402343e03b0d812dc48ce680b829e5322d6aa1c
Michel Lujano <lvm3632@gmail.com>
2020-12-19 13:32:32 -0600

    Setting admin page

--- app.js
+++ app.js
@@ -6,12 +6,20 @@ const exphbs = require("express-handlebars");
 
 app.use(express.static(path.join(__dirname, "public")));
 
+// Set View Engine
+
 app.engine("handlebars", exphbs({ defaultLayout: "home" }));
 app.set("view engine", "handlebars");
 
-const main = require("./routes/home/main");
+// Load Routes
 
-app.use("/", main);
+const home = require("./routes/home/index");
+const admin = require("./routes/admin/index");
+
+// Use Routes
+
+app.use("/", home);
+app.use("/admin", admin);
 
 app.listen(4500, () => {
   console.log(`listening on port 4500`);
